287b82d012a1a0041baca9f70b22d5c5d0d733d2
Joe <Joe@10.89.137.117>
2015-02-17 13:43:35 +0100

    First commit

---
+++ src/main/java/edu/upc/eeatc/dsa/jalvarez/hellogit/App.java
@@ -0,0 +1,13 @@
+package edu.upc.eeatc.dsa.jalvarez.hellogit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
